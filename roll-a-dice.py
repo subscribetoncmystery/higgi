f64aa5470fcc4f361ba9b3dc5953c89b4dfc3f18
subscribetoncmystery <140418907+subscribetoncmystery@users.noreply.github.com>
2023-07-24 20:01:06 +0530

    Add files via upload

---
+++ roll-a-dice.py
@@ -0,0 +1,43 @@
+no = random.randint(1,6)
+if no == 1:
+print("[--------]"),
+print("[        ]"),
+print("[    1    ]"),
+print("[        ]"),
+print("[--------]")
+
+if no == 2:
+print("[--------]"),
+print("[        ]"),
+print("[    2    ]"),
+print("[        ]"),
+print("[--------]")
+
+
+if no == 3:
+print("[--------]"),
+print("[        ]"),
+print("[    3    ]"),
+print("[        ]"),
+print("[--------]")
+
+if no == 4:
+print("[--------]"),
+print("[        ]"),
+print("[    4    ]"),
+print("[        ]"),
+print("[--------]")
+
+if no == 5:
+print("[--------]"),
+print("[        ]"),
+print("[    5    ]"),
+print("[        ]"),
+print("[--------]")
+
+if no == 6:
+print("[--------]"),
+print("[        ]"),
+print("[    6    ]"),
+print("[        ]"),
+print("[--------]")
